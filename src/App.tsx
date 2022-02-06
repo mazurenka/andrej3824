bd2a499c29f3b44670468eab4d192d029ff02d06
mazurenka <an.drej@mail.ru>
2022-02-06 15:16:23 +0100

    React.lazy и React.Suspense, зачем? #94 - ReactJS

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import {Navbar} from "./components/Navbar/Navbar";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import {StoreType} from "./redux/store"
 import UsersContainer from "./components/Users/UsersContainer";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import ProfileContainer from "./components/Profile/ProfileContainer";
+
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+
+//import ProfileContainer from "./components/Profile/ProfileContainer";
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -36,10 +41,16 @@ class App extends Component {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
+                           render={() =>{
+                               return  <React.Suspense fallback={<div>Loading...</div>}>
+                                   <DialogsContainer/>
+                               </React.Suspense>}}/>
 
                     <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
+                           render={() => {
+                               return <React.Suspense fallback={<div>Loading...</div>}>
+                                   <ProfileContainer/>
+                               </React.Suspense>}}/>
                     <Route path={'/users'}
                            render={() => <UsersContainer pageTitle={'Samurai'} />}/>
                     <Route path={'/login'}
