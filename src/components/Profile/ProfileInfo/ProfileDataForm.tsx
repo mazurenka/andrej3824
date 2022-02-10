7fba1abf85465cd5d3092e2b3a7caeba4bb5713d
mazurenka <an.drej@mail.ru>
2022-02-10 18:50:54 +0100

    97 - ReactJS - обновление профиля

--- src/components/Profile/ProfileInfo/ProfileDataForm.tsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import {createField, Input, Textarea} from "../../common/FormsControls/FormsControls";
+import {reduxForm} from "redux-form";
 
 const ProfileDataForm = ({profile}) => {
-    return <div>
-        Form
-    </div>
+    return <form>
+        <div>
+            <button onClick={() => {
+            }}>save
+            </button>
+        </div>
+        <div>
+            <b>Full name</b>: {createField('Full name', 'fullname', [], Input)}
+        </div>
+        <div>
+            <b>Looking for a job</b>: {createField("", "lookingForAJob", [], Input, {type: 'checkbox'})}
+        </div>
+
+        <div>
+            <b>My professional skills</b>:
+            {createField("My professional skills", "lookingForAJobDescription", [], Textarea)}
+        </div>
+
+        <div>
+            <b>About me</b>: {profile.aboutMe}
+            {createField("About me", "aboutMe", [], Textarea)}
+        </div>
+        <div>
+            <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+            return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+        })}
+        </div>
+    </form>
 }
 
-export default ProfileDataForm
+const ProfileDataFormReduxForm = reduxForm({form: 'edit-profile'})(ProfileDataForm)
+
+export default ProfileDataFormReduxForm
