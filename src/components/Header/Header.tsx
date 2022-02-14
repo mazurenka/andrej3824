5d9a612f2101348f195ca5ba2171565426a963b3
mazurenka <an.drej@mail.ru>
2022-02-14 19:56:50 +0100

    99 - ReactJS - try-catch, router switch, classnames

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,24 @@ import s from './Header.module.css';
 export type MapPropsType = {}
 
 export const Header: React.FC<MapPropsType> = (props) => {
+
+    const isAuth = useSelector(selectIsAuth)
+    const login = useSelector(selectCurrentUserLogin)
+
+    const dispatch = useDispatch()
+
+    const logoutCallback = () => {
+        dispatch(logout())
+    }
+
+    const {Header} = Layout
+
     return <header className={s.header}>
         <div>
             <img
                 src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo5N9H84BlUK8bkFMOdtEP6km4nqGKWcLBZQ&usqp=CAU'}/>
             <div className={s.loginBlock}>
-                {props.isAuth
+                {isAuth
                     ? <div> {props.login} - <button onClick={props.logout}>Log out</button></div>
                     : <NavLink to={'/login'}>LOGIN</NavLink>}
             </div>
