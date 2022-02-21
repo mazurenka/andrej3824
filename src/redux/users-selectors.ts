919502ab19200f35576b19c00178286536f7983c
mazurenka <an.drej@mail.ru>
2022-02-21 18:26:22 +0100

    11 - React + TypeScript / Типизируем ВСЁ / React JS - Путь Самурая 2.0

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -4,11 +4,9 @@ import {AppStateType} from "./redux-store";
 const getUsersSelector = (state: AppStateType) => {
     return state.usersPage.users
 }
-
 export const getUsers = createSelector(getUsersSelector, (users) => {
     return users.filter(u => true)
 })
-
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize
 }
@@ -25,10 +23,6 @@ export const getFollowingInProgress = (state: AppStateType) => {
     return state.usersPage.followingInProgress
 }
 
-export const countSomethingDifficult = (state: AppStateType) => {
-    let count = 23
-    return count
-}
 
 
 
