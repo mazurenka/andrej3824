bde03d058703f8c6c86533c22c9c3c71c0db89f6
mazurenka <an.drej@mail.ru>
2022-03-12 22:02:17 +0100

    18 - WebSocket + React, часть 1/ React JS - Путь Самурая 2.0

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -35,14 +35,13 @@ const Messages: React.FC = () => {
 
 
     return <div style={{height: '400px', overflow: 'auto'}}>
-        {messages.map((m) => <Message message={m}/>)}
+        {messages.map((m, index) => <Message key={index} message={m}/>)}
     </div>
 }
 
 const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
-
     return <div>
-        <img src={message.photo}/> <b>{message.userName}</b>
+        <img src={message.photo} style={{width: '30px'}}/> <b>{message.userName}</b>
         <br/>
         {message.message}
         <hr/>
